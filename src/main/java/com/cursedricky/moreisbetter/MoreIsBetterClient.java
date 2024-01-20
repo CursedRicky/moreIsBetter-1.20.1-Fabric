e062dab8855b15a290c99800047de6841ebb55ee
CursedRicky <78096957+CursedRicky@users.noreply.github.com>
2024-01-20 19:18:27 +0100

    Initial commit

---
+++ src/main/java/com/cursedricky/moreisbetter/MoreIsBetterClient.java
@@ -0,0 +1,10 @@
+package com.cursedricky.moreisbetter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreIsBetterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/cursedricky/moreisbetter/MoreIsBetterDataGenerator.java
@@ -0,0 +1,11 @@
+package com.cursedricky.moreisbetter;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoreIsBetterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
